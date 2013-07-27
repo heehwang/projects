34c29360377b06b634aaced5ad0cb45d74ab04d5
Hee Hwang <heehwang@ucla.edu>
2013-07-26 20:36:35 -0700

    test

--- CS61BL/day16/BinarySearchTreeTest.java
+++
@@ -1,7 +0,0 @@
-import junit.framework.TestCase;
-
-public class BinarySearchTreeTest extends TestCase {
-	BinaryTree t = new BinaryTree();
-	t.fillSampleTree1();
-
-}
--- CS61BL/lab7/Dog.java
+++
@@ -1,9 +0,0 @@
-
-public class Dog extends Animal{
-	public void method2() {
-		System.out.println("Dog: method2");
-	}
-	public void method4() {
-		System.out.println("Dog: method4");		
-	}
-}
